6d06d0696cf5d0ed9ee97e672b0e6ee538d8e9e1
Itay <itayh@sensecom.co.il>
2020-09-09 11:52:40 +0300

    example 2 finished, example 1 removed

--- maakavOnlineExample2/src/app/components/main/main.component.ts
+++ maakavOnlineExample2/src/app/components/main/main.component.ts
@@ -9,7 +9,39 @@ export class MainComponent implements OnInit {
 
   constructor() { }
 
+  allProvs = [
+    {
+      code: 1,
+      name: "Netvision"
+    },
+    {
+      code: 2,
+      name: "Partner"
+    },
+    {
+      code: 3,
+      name: "Bezeq"
+    }
+  ]
+
   ngOnInit(): void {
   }
 
+  renderNetvision()
+  {
+    let netvisionText = "Example instructional text for Netvision"
+    document.getElementById("content").innerText = netvisionText;
+  }
+
+  renderPartner()
+  {
+    let partnerText = "Example instructional text for Partner"
+    document.getElementById("content").innerText = partnerText;
+  }
+
+  renderBezeq()
+  {
+    let bezeqText = "Example instructional text for Bezeq"
+    document.getElementById("content").innerText = bezeqText;
+  }
 }
